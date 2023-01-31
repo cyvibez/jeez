15340779aa84c41a6876d7ab9b7895e7accbc028
cyvibez <cyprusakanni@gmail.com>
2023-01-31 21:41:45 +0100

    checked because of the dark/light toggle'

--- libraryScript.js
+++ libraryScript.js
@@ -14,12 +14,35 @@ function closeFullDisplay(){
 // dark and light mode 
 
 var icon = document.getElementById("icon")
+// var icon2 = document.getElementById("icon2")
 
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme")
     if (document.body.classList.contains("dark-theme")) {
-        icon.src = "images/sun.png";
-    } else {
-        icon.src = "images/moon.png";
+        icon.src = "images/sun.png" ;
+    }else{
+        icon.src = "images/moon.png" ;
     }
-}
+ }
+
+
+
+// icon2.onclick = function(){
+//     document.body.classList.toggle("light-theme")
+//     icon2.style.display = "none";
+// }
+    // if (document.body.classList.contains("dark-theme")) {
+    //     icon2.style.display = "flex";
+    // } else{
+    //     icon.style.display = "flex";
+    // }
+    // }
+
+// icon2.onclick = function(){
+//     document.body.classList.toggle("light-theme")
+//     if (document.body.classList.contains("light-theme")) {
+//         icon.style.display = "flex";
+//     } else {
+//         icon2.style.display = "none";
+//     }
+// }
