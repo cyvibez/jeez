9c7166e273735d22ced5ac97548b2ded35e05806
cyvibez <cyprusakanni@gmail.com>
2023-01-31 17:41:52 +0100

    updated images, header and footer

--- libraryScript.js
+++ libraryScript.js
@@ -1,3 +1,5 @@
+// Full image dispaly
+
 var  fullDisplay = document.getElementById('fullDisplay');
 var fullImg = document.getElementById('fullImg')
 
@@ -7,4 +9,17 @@ function openFullDisplay(full){
 }  
 function closeFullDisplay(){
     fullDisplay.style.display = "none";
+}  
+
+// dark and light mode 
+
+var icon = document.getElementById("icon")
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme")
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.png";
+    } else {
+        icon.src = "images/moon.png";
+    }
 }
